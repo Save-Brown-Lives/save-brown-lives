e0f4f3ccf086f0f0774f77f5fabe9036d4a7cba3
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-12 19:56:26 -0800

    added code for one API endpoint

--- client/src/pages/Resources.jsx
+++ client/src/pages/Resources.jsx
@@ -1,10 +1,38 @@
 // pages/Resources.jsx
 //import useState fron React
-import { useState } from "react";
+import { useEffect, useState } from "react";
+//import components
+import Card from "../components/Card.jsx";
 export default function Resources() {
+  const [legalInfo, setLegalInfo] = useState([]);
+  //formData
   const [formData, setFormData] = useState({
     zipcode: "",
   });
+
+  //Declare an asynchronous arrow function to get all saved countries from the server
+  const getLegalInfo = async (zipcode) => {
+    try {
+      //Declare  a variable that will hold response from the GET request to /get-all-saved-countries
+      const response = await fetch(`/api/get-legal-help/${zipcode}`);
+      //Guard Clause
+      if (!response.ok) {
+        console.error(`Response status: ${response.status}`);
+        // Exit early
+        return;
+      }
+      //Convert the response to JSON format using json() method and save it in a variable named data
+      const data = await response.json();
+      //print data on console
+      console.log("lawyers data ", data);
+
+      //set lawyer data using the setter function
+      setLegalInfo(data);
+    } catch (error) {
+      //Print error on console
+      console.log("Error retrieving legal resources:" + error.message);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -15,15 +43,18 @@ export default function Resources() {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData, "formData");
+    console.log("zip :", formData.zipcode);
+    getLegalInfo(formData.zipcode);
     setFormData({ zipcode: "" });
   };
   return (
     <>
       <h2> Sister Organizations</h2>
       {/* //add a list f national organizations  devoted towards rights of poc*/}
+
+      <h2>Find resources in your area : </h2>
       <form className="zip-form" onSubmit={handleSubmit}>
-        <label for="zip">Zip Code</label>
+        <label htmlFor="zipcode">Zip Code</label>
         <input
           id="zipcode"
           type="text"
@@ -35,6 +66,7 @@ export default function Resources() {
         />
         <button type="submit"> Submit</button>
       </form>
+      <div className="results"></div>
     </>
   );
 }
--- server/src/index.js
+++ server/src/index.js
@@ -4,14 +4,22 @@
 
 //Importing all of our node modules
 import express from "express"; // the framework that lets us build webservers
+import pg from "pg";
+import config from "./config.js"; //importing the connection string to our database hosted on NEON
 
-//Declare a variable named app and call the express() function to create a new instance of express so we can use all of the methods, fucntions, properties of express
-// which will be saved in app
-const app = express(); // this gives us access to all of Express's functions, methods, useful superpowers
+//connecting to our PostgreSQL database , or db for short
+const db = new pg.Pool({
+  //new pg.Pool creates a connection to the database
+  connectionString: config.databaseUrl, //this is the credentials to access the database. Keep private
+  ssl: true, // use SSL encryption when connecting to the database to keep data safe in transit
+});
+
+//create an instance of express
+const app = express();
 
 //Defining out port number
 //What port should our server listen to?
-const port = 3000; // you can use any port # but developers commonly use 3000. also there are some port numbers you cannot use
+const port = 3000;
 
 //Declaring that this server will be receiving and responding to requests in JSON
 app.use(express.json()); // this server will receive and respond to requests with JSON data
@@ -24,4 +32,29 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.send("Hi, Server is ON!");
+});
+
+/*----------------------------------
+Helper functions
+----------------------------------*/
+
+//getlawyers()
+const getLegalHelp = async (zipcode) => {
+  const data = await db.query("SELECT * FROM legal_firms WHERE zipcode = $1;", [
+    zipcode,
+  ]);
+  console.log(data.rows);
+  return data.rows;
+};
+
+/*----------------------------------
+API Endpoints
+----------------------------------*/
+
+//	GET	/get-legal-help/:zipcode	Retrieves lawyer at the zipcode
+app.get("/get-legal-help/:zipcode", async (req, res) => {
+  let zipcode = req.params.zipcode;
+  //call helper function
+  let lawyers = await getLegalHelp(zipcode);
+  res.json(lawyers);
 });
